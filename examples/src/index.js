a5c4c6aa308ef2f3404fdac74480f27de5baae62
shibe97 <k.shibata97@gmail.com>
2019-09-07 21:00:26 +0900

    add example

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from 'react';
 import { render } from 'react-dom';
 import Modal from '../../src';
+import styles from './styles.module.css';
 
 const App = () => {
   const [isOpen, setOpen] = useState(false);
@@ -10,7 +11,10 @@ const App = () => {
     <div>
       <button onClick={open}>OPEN</button>
       <Modal isOpen={isOpen} close={close}>
-        modal
+        <div className={styles.modal}>
+          <h1>Title</h1>
+          <p>This is a react-portal-modal.</p>
+        </div>
       </Modal>
     </div>
   );
